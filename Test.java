6b7854f92ffe2d8b981a7e122d42de6935f92bbb
Caio Nascimento Maia <camaia@uolinc.com>
2021-05-29 21:45:18 -0300

    Fix bug

--- Test.java
+++ Test.java
@@ -1,5 +1,10 @@
 public class Test {
   public static void main(String[] args) {
-	System.out.println("bug!");
+	execute();
+  }
+
+  static void executa() {
+    System.out.println("agora ficou pior");
+
   }
 }
