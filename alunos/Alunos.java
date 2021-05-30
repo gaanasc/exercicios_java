b6c97ab173b7c2fdf1e8f3ab4e7ea28bd5efbd1a
gaanasc <gabriel_nasc@hotmail.com>
2021-05-30 00:43:22 -0300

    Apresentando os dados de alunos com o System.out

---
+++ alunos/Alunos.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Alunos {
+    public String nome;
+    public int matricula;
+    private String cpf;
+    public String email;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
